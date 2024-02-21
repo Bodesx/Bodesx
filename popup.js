391ffebe2983eac5a9ec8a84516c2e7905d12dda
Bode Lawanson <104658283+Bodesx@users.noreply.github.com>
2024-02-21 11:38:50 +0100

    2/21/24

--- popup.js
+++ popup.js
@@ -9,3 +9,18 @@ $('.close-video').click(function(){
         $('.video-popup').fadeOut();
         $('#iframeHolder').html('');
     });
+
+
+
+     function Circlle(el) {
+            $(el).circleProgress({
+                    fill: {
+                        color: '#0F4755'
+                    }
+                })
+                .on('circle-animation-progress', function(event, progress, stepValue) {
+                    $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2) + '%');
+                });
+        };
+
+        Circlle('.round');
